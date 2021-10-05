67565deca4c24e6738dfd5a26846b76ece5f831f
nuaalixu <nuaalixu@outlook.com>
2021-10-05 15:48:55 +0800

    support subroutine

--- pyos/task.py
+++ pyos/task.py
@@ -1,4 +1,8 @@
 """A task is a wrapper around a coroutine."""
+from typing import Generator
+
+from pyos.systemcall import SystemCall
+
 
 class Task(object):
     """This object encapsulates a running task."""
@@ -10,8 +14,28 @@ class Task(object):
         self.tid     = Task.taskid   # Task ID
         self.target  = target        # Target coroutine
         self.sendval = None          # Value to send
+        self.stack   = []            # Call stack
 
     
     def run(self):
         """ Run a task until it hits the next yield statement."""
-        return self.target.send(self.sendval)
+        while True:
+            try:
+                result = self.target.send(self.sendval)
+                # if it's a systemcall, just return
+                if isinstance(result, SystemCall): return result
+                # if it's a generator, we're going to "trampoline"
+                if isinstance(result, Generator):
+                    self.stack.append(self.target)
+                    self.sendval = None
+                    self.target = result
+                # if some other value is combing back, assume it's a return value from a subroutine
+                else:
+                    if not self.stack: return
+                    self.sendval = result
+                    self.target = self.stack.pop()
+            # if subroutines terminate, pop the last coroutine off the stack or kill the whole task
+            except StopIteration:
+                if not self.stack: raise
+                self.sendval = None
+                self.target = self.stack.pop()
---
+++ test/test_subroutine.py
@@ -0,0 +1,46 @@
+from socket import *
+from pyos import *
+from pyos.systemcall import *
+
+
+def Accept(sock):
+    yield ReadWait(sock)
+    yield sock.accept()
+
+
+def Send(sock,buffer):
+    while buffer:
+        yield WriteWait(sock)
+        len = sock.send(buffer)
+        buffer = buffer[len:]
+
+
+def Recv(sock,maxbytes):
+    yield ReadWait(sock)
+    yield sock.recv(maxbytes)
+
+
+def handle_client(client,addr):
+    print("Connection from", addr)
+    while True:
+        data = yield Recv(client,65536)
+        if not data:
+            break
+        yield Send(client,data)
+    print("Client closed")
+    client.close()
+
+
+def server(port):
+    print("Server starting")
+    sock = socket(AF_INET,SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+    sock.bind(("",port))
+    sock.listen(5)
+    while True:
+        client,addr = yield Accept(sock)
+        yield NewTask(handle_client(client,addr))
+
+sched = Scheduler()
+sched.new(server(45000))
+sched.mainloop()
